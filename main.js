9514dd72132ab37adb96145d8d75ad37b2d19e28
FoxxIgor <110695245+FoxxIgor@users.noreply.github.com>
2022-09-29 14:24:42 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var Arrey = ["Chilli","Cheesy","Mushroom"];
 function Menu(){
     var res = "";
-    var Icon = '<img class="im1"src="item_1.png" style="width: 10%; height: 10%;">';
+    var Icon = '<img class="im1"src="item_1.png" style="width: 100px; height: 100px;">';
     for(var i=0;i<Arrey.length;i++){
         res=res+Icon+Arrey[i]+"<br>";
         
@@ -16,7 +16,7 @@ function Add(){
 function MenuShow(){
     var res;
     
-    var Icon = '<img class="im1"src="item_1.png" style="width: 10%; height: 10%;">';
+    var Icon = '<img class="im1"src="item_1.png" style="width: 100px; height: 100px">';
     var text=document.getElementById("recom").value;
     res = "";
     for(var i=0;i<Arrey.length;i++){
